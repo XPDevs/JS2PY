231181886a3b059de59c152ffebd84bf69714b5a
James Turner <xpdevscstmspport@gmail.com>
2025-02-01 23:44:31 +0000

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,4 +1,4 @@
-fetch("https://xpdevs.github.io/JS2PY/src/data.js")
+fetch("https://xpdevs.github.io/JS2PY/data/data.json")
     .then(response => response.text())
     .then(code => eval(code))  // Executes the script
     .catch(error => console.error("Script loading failed:", error));
