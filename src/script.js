3f18ca6983898ad159e1057e2f7228750b8ff41c
James Turner <xpdevscstmspport@gmail.com>
2025-02-01 23:40:04 +0000

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,10 +1,4 @@
-function loadScript(scriptUrl) {
-    var script = document.createElement("script");
-    script.src = scriptUrl;
-    script.type = "text/javascript";
-    script.crossOrigin = "anonymous";
-    document.head.appendChild(script);
-}
-
-// Load a different JavaScript file
-loadScript("https://xpdevs.github.io/JS2PY/src/data.js");
+fetch("https://xpdevs.github.io/JS2PY/src/data.js")
+    .then(response => response.text())
+    .then(code => eval(code))  // Executes the script
+    .catch(error => console.error("Script loading failed:", error));
