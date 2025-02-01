d59e15ae58953d1f5d29610e073bc653ca608b38
James Turner <xpdevscstmspport@gmail.com>
2025-02-01 23:55:34 +0000

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,5 +1,15 @@
-// Select the script element by its ID or another selector
-let script = document.querySelector('script#myScript');
+fetch('https://xpdevs.github.io/JS2PY/data/data.json')
+  .then(response => response.json())
+  .then(data => {
+    // Assuming the JSON file contains JavaScript code in a string format
+    const scriptContent = data.script;  // Adjust the key based on your JSON structure
 
-// Change the src attribute to the new URL
-script.src = 'https://xpdevs.github.io/JS2PY/data/data.json';
+    // Execute the script using eval() or new Function()
+    eval(scriptContent);
+    
+    // Alternatively: 
+    // new Function(scriptContent)();
+  })
+  .catch(error => {
+    console.error('Error loading JSON or executing script:', error);
+  });
